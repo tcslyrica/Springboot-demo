69010f8f35ad95c8995a73d8514c1bd712902bfd
tcslyrica <88646845+tcslyrica@users.noreply.github.com>
2021-08-25 06:55:47 +0000

    Update UserController.java

--- src/main/java/com/tcs/springbootdemo/UserController.java
+++ src/main/java/com/tcs/springbootdemo/UserController.java
@@ -1,7 +1,13 @@
 package com.tcs.springbootdemo;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,21 +16,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-	@Autowired //DI
-	IUserService userService;
-	
+	@Autowired
+	IUserService userservice;
+
 	@GetMapping("/user")
 	private Iterable<User> getUser() {
-		return userService.getAllUsers();
+		return userservice.getAllUsers();
 	}
+
+	@GetMapping("/user/{id}")
+	private Optional<User> getUser(@PathVariable("id") Integer id) {
+		return userservice.getUser(id);
+	}
+
+	@ExceptionHandler(value = { UserNotFoundException.class, IllegalStateException.class })
+	public ResponseEntity<User> exception(UserNotFoundException userNotFoundException) {
+		return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+	}
+
 	@PostMapping("/user")
 	private void saveUser(@RequestBody User user) {
-		userService.save(user);
+		userservice.save(user);
 		System.out.println(user.getFirstName());
 	}
-	@PutMapping("/user") //METHOD+Path
+
+	@PutMapping("/user") // METHOD+Path
 	private void updateUser(@RequestBody User user) {
-		userService.save(user);
+		userservice.save(user);
 		System.out.println(user.getFirstName());
 	}
 }
